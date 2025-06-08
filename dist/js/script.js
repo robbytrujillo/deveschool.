8cdd682e4e8ecd310b77a6deb2d11fb378cf437a
robbytrujillo <robbyikusuma88@gmail.com>
2025-06-09 06:04:58 +0700

    Introduction Developer School Mastery

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,3 +1,10 @@
+const hamburger = document.querySelector(".ri-menu-line");
+const menu = document.querySelector(".menu");
+
+hamburger.addEventListener("click", () => {
+    menu.classList.toggle("menu-active");
+});
+
 const scrollUp = document.querySelector(".scroll-up");
 
 window.addEventListener("scroll", () => {
@@ -6,4 +13,4 @@ window.addEventListener("scroll", () => {
     } else {
         scrollUp.classList.remove("scroll-active");
     }
-})
+});
